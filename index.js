7e25233dc6a5db4fb33f89a18e8907a92d52b958
imthesk01 <64018806+imthesk01@users.noreply.github.com>
2020-11-11 11:56:42 +0530

    Add files via upload
    JQuery DOM program

---
+++ index.js
@@ -0,0 +1,14 @@
+function hover1(){
+	$("#item1-des").animate(slideToggle());
+
+};
+function hover2(){
+	$("#item2-des").animate(slideToggle());  
+};
+function animate_big(){
+	$("#buy").animate(width("200%"));
+  //  $("div").width("50%");
+};
+function animate_small(){
+  $("#buy").animate(width("100%"));
+};
